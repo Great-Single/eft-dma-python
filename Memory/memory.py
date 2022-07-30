05a28152d7cea2a589ce744e5a9146ec3335f0b4
Jacob Hamblin <jacob@hamblin.cc>
2022-07-30 14:26:17 -0700

    check for process

--- Memory/memory.py
+++ Memory/memory.py
@@ -9,8 +9,10 @@ class Memory:
         self.tarkov_process = False
         self.mmap_exists = self.check_for_mmap()
         self.vmm = self.initialize_dma()
+        self.process_check_timer = threading.Timer(1.0, self.check_for_process)
 
     def initialize_dma(self):
+        print("initializing memory access over DMA")
         if self.mmap_exists:
             self.vmm = memprocfs.Vmm(["-device", "fpga", "-mmap", "mmap.txt"])
         else:
@@ -44,9 +46,10 @@ class Memory:
             print("would check for process, but no dma connection initialized")
             return False
         print("checking for tarkov process")
-        threading.Timer(1.0, self.check_for_process).start()
+        self.process_check_timer.start()
         tarkov_process = self.vmm.process("EscapeFromTarkov.exe")
         if tarkov_process:
             print("found tarkov process")
             self.tarkov_process = tarkov_process
+            self.process_check_timer.stop()
         return False
--- OperationsManager/operations_manager.py
+++ OperationsManager/operations_manager.py
@@ -3,9 +3,6 @@ from Memory.memory import Memory
 
 class OperationsManager:
     def __init__(self):
-        self.memory = self.memory_init()
-
-    def memory_init(self):
-        print("<(o.o<) welcome")
-        print("initializing memory access over DMA")
+        print("<(o.o <) welcome")
         self.memory = Memory()
+        self.memory.check_for_process()
